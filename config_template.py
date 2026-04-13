db4e484b8612d5a9db119d6f150ceae8c75b57f5
aurelienaberlenc <aberlenc.aurelien@gmail.com>
2026-04-13 15:04:56 +0200

    gitignore + config template

--- config_template.py
+++ config_template.py
@@ -1,11 +1,12 @@
 from pathlib import Path
 from spikeinterface.sorters import get_default_sorter_params
 
-DATA_ROOT = Path("F:/Data_Mice_IBL")
+DATA_ROOT = Path("path/to/your/data")
+DB_PATH = DATA_ROOT / "your_db.feather"
 
 KS_PARAMS = get_default_sorter_params("kilosort4")
 
-COMPRESS_KEEP_ORIGINAL = False
+COMPRESS_KEEP_ORIGINAL = True
 KS_REMOVE_EXISTING_FOLDER = True
 
 ANALYZER_N_JOBS = 1
